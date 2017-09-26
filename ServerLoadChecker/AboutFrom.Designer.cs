31c75335c874417dc77b318cd4d74e64973d324c
mengqingyu <meng352247816@outlook.com>
2017-09-26 13:17:23 +0800

    新增了一个项目
    新增了一个项目

---
+++ ServerLoadChecker/AboutFrom.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ServerLoadChecker
+{
+    partial class AboutFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(0, 1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 187);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "\r\n\r\n未经本人允许，任何人不得用于商业用途！\r\n作者联系方式：\r\nmeng352247816@outlook.com";
+            // 
+            // AboutFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 188);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "AboutFrom";
+            this.Text = "关于";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
